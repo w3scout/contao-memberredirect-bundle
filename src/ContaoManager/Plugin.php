52739106e0b8ab6d1ef225680f7cdc0508124b02
Darko Selesi <hallo@w3scouts.com>
2017-10-28 06:52:54 +0200

    Update copyright notice

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -1,11 +1,28 @@
 <?php
+
 /**
- * @copyright  Darko Selesi 2017 <http://w3scouts.com>
- * @author     Darko Selesi (w3scout)
- * @package    Addresses
- * @license    LGPL-3.0+
- * @see	       https://github.com/w3scout/spotify.git
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2011 Leo Feyer
  *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * @copyright  w3scouts.com
+ * @author     Darko Selesi
+ * @license    LGPL
  */
 
 namespace W3Scout\MemberredirectBundle\ContaoManager;
